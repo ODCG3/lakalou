6887ca56f387c3d3c5b68686c07ea94b8121e510
bakemono-san <mousdef34@gmail.com>
2024-08-06 09:04:58 +0000

    mis a jour du us post(ajout de la recuperation de donnees)

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,3 +1,4 @@
+import model from "../models/ModelModel.js";
 import post from "../models/PostModel.js";
 
 export default class PostController {
@@ -17,6 +18,58 @@ export default class PostController {
             });
             res.status(201).json(createdPost);
         } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+
+    static async getAllPosts(req, res) {
+        try {
+            const posts = await post.find({});
+            res.json(posts);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+
+    static async getPostById(req, res) {
+
+        try {
+            const postData = await post.findById(req.params.id);
+            console.log(postData, req.params.id);
+            
+            if (!postData) {
+                return res.status(404).json({ error: 'Post not found' });
+            }
+            res.json(postData);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+
+    static async deletePost(req, res) {
+        const { id } = req.params.id;
+
+        try {
+            const deletedPost = await post.findByIdAndDelete(id);
+            if (!deletedPost) {
+                return res.status(404).json({ error: 'Post not found' });
+            }
+            res.json(deletedPost);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+
+    static async getModel(req, res) {
+        const postId = req.params.id;
+
+        try{
+            const postData = await post.findById(postId);
+            const modelData = await model.findById(postData.model);
+
+            res.json(modelData);
+        }
+        catch (error) {
             res.status(500).json({ error: error.message });
         }
     }
