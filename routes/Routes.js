20b6ec6530bafe66c06a680f7f05cb96f06f384c
Dimkha <dimkha516@gmail.com>
2024-08-06 21:28:40 +0000

    lister mes followers

--- routes/Routes.js
+++ routes/Routes.js
@@ -3,11 +3,13 @@ import UserController from "../controllers/UserController.js";
 import auth from "../middlewares/auth.js";
 import ModelController from "../controllers/ModelController.js";
 import PostController from "../controllers/PostController.js";
-import StoryController from '../controllers/StoryController.js';
+import StoryController from "../controllers/StoryController.js";
 
-import { likePost, unlikePost, getPostLikes } from '../controllers/LikeController.js';
-
-
+import {
+  likePost,
+  unlikePost,
+  getPostLikes,
+} from "../controllers/LikeController.js";
 
 const router = express.Router();
 
@@ -18,7 +20,6 @@ router.route("/login").post((req, res) => UserController.login(req, res));
 router.route("/logout").post((req, res) => UserController.logout(req, res));
 
 router.route("/test").get(auth, (req, res) => UserController.test(req, res));
-
 
 // FOLLOW USER
 
@@ -31,64 +32,74 @@ router
   .route("/unfollow/:id")
   .post((req, res) => UserController.unfollowUser(req, res));
 
-router.route('/add-note/:id')
-    .post(auth, (req, res) => UserController.addNote(req, res));
+router
+  .route("/add-note/:id")
+  .post(auth, (req, res) => UserController.addNote(req, res));
 
-router.route('/signal/:id')
-    .post(auth, (req, res) => UserController.reportUser(req, res));
+router
+  .route("/signal/:id")
+  .post(auth, (req, res) => UserController.reportUser(req, res));
 
 // Model routes
-router.route('/model/create')
-    .post(auth, (req, res) => ModelController.create(req, res));
+router
+  .route("/model/create")
+  .post(auth, (req, res) => ModelController.create(req, res));
 
 // Post routes
-router.route('/post/create')
-    .post(auth, (req, res) => PostController.create(req, res));
+router
+  .route("/post/create")
+  .post(auth, (req, res) => PostController.create(req, res));
 
-router.route('/post')
-    .get(auth, (req, res) => PostController.getAllPosts(req, res));
+router
+  .route("/post")
+  .get(auth, (req, res) => PostController.getAllPosts(req, res));
 
-router.route('/post/:id')
-    .get(auth, (req, res) => PostController.getPostById(req, res));
+router
+  .route("/post/:id")
+  .get(auth, (req, res) => PostController.getPostById(req, res));
 
-router.route('/post/:id/model')
-    .get(auth, (req, res) => PostController.getModel(req, res));
+router
+  .route("/post/:id/model")
+  .get(auth, (req, res) => PostController.getModel(req, res));
 
 // Story routes
-router.route('/story/create')
-    .post(auth, StoryController.createStory);
+router.route("/story/create").post(auth, StoryController.createStory);
 
+router
+  .route("/profile")
+  .get(auth, (req, res) => UserController.profile(req, res));
 
-router.route('/profile')
-    .get(auth, (req, res) => UserController.profile(req, res));
+router
+  .route("/post/favorite/create/:id")
+  .post(auth, (req, res) => PostController.addFavorite(req, res));
 
+router
+  .route("/post")
+  .get(auth, (req, res) => PostController.getAllPosts(req, res));
 
-router.route('/post/favorite/create/:id')
-    .post(auth, (req, res) => PostController.addFavorite(req, res));
+router
+  .route("/post/:id")
+  .get(auth, (req, res) => PostController.getPostById(req, res));
 
+router
+  .route("/post/:id/model")
+  .get(auth, (req, res) => PostController.getModel(req, res));
 
-router.route('/post')
-.get(auth, (req, res) => PostController.getAllPosts(req, res));
-
-router.route('/post/:id')
-    .get(auth, (req, res) => PostController.getPostById(req, res));
-
-router.route('/post/:id/model')
-        .get(auth, (req, res) => PostController.getModel(req, res));
-
-
-        
-router.route('/test')
-    .get(auth, (req, res) => UserController.test(req, res));
+router.route("/test").get(auth, (req, res) => UserController.test(req, res));
 
 // Route pour aimer un post
 /* router.post('/post/:postId/like', likePost); */
-router.post('/post/:postId/like', auth, likePost);
+router.post("/post/:postId/like", auth, likePost);
 
 // Route pour ne pas aimer un post
-router.post('/post/:postId/like/:likeID/unlike',auth, unlikePost);
+router.post("/post/:postId/like/:likeID/unlike", auth, unlikePost);
 
 // Route pour récupérer les likes d'un post
-router.get('/post/:postId/likes',auth, getPostLikes);
+router.get("/post/:postId/likes", auth, getPostLikes);
 
-export default router; 
+// ROUTE POUR AFFICHER LES FOLLOWERS D'UN TAILLEUR:
+router
+  .route("/post/myFollowers")
+  .post(auth, (req, res) => UserController.myFollowers(req, res));
+
+export default router;
