5a2f67ed5853a2efd42dd542da1afb8ef4fafe27
bakemono-san <mousdef34@gmail.com>
2024-08-20 09:11:46 +0000

    update userController

--- dist/PrismaUserController.d.ts
+++ dist/PrismaUserController.d.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 export default class PrismaUserController {
     static create(req: Request, res: Response): Promise<Response<any, Record<string, any>> | undefined>;
     static login(req: Request, res: Response): Promise<Response<any, Record<string, any>> | undefined>;
@@ -9,6 +9,7 @@ export default class PrismaUserController {
     static followUser(req: Request, res: Response): Promise<Response<any, Record<string, any>>>;
     static profile(req: Request, res: Response): Promise<Response<any, Record<string, any>>>;
     static changeRole(req: Request, res: Response): Promise<Response<any, Record<string, any>>>;
+    static updateMeasurements(req: Request, res: Response): Promise<Response<any, Record<string, any>>>;
     static bloquerUsers(req: Request, res: Response): Promise<Response<any, Record<string, any>>>;
     static debloquerUsers(req: Request, res: Response): Promise<Response<any, Record<string, any>>>;
     static updateNote(req: Request, res: Response): Promise<Response<any, Record<string, any>> | undefined>;
@@ -16,6 +17,4 @@ export default class PrismaUserController {
     static getTailleurs(req: Request, res: Response): Promise<void>;
     static myPosition(req: Request, res: Response): Promise<void>;
     static getTailleurRanking(req: Request, res: Response): Promise<void>;
-    static updateMeasurements(req: Request, res: Response): Promise<Response<any, Record<string, any>>>;
-    static getStatistiques(req: Request, res: Response): Promise<Response<any, Record<string, any>> | undefined>;
 }
