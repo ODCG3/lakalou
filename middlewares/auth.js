73c9a988c1ee6d88fced4009b65df5313f9f3b38
bakemono-san <mousdef34@gmail.com>
2024-10-08 18:12:47 +0000

    feat(backend) : updated some backend functionnality

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,8 @@ import jwt from 'jsonwebtoken';
 
 
 export default (req, res, next) => {
-    const token = req.cookies.token ? req.cookies.token : null;
+    // const token = req.cookies.token ? req.cookies.token : null;
+    const token = req.cookies.token || req.headers['authorization']?.split(' ')[1];
     //console.log(token);
     if (!token) {
         return res.status(401).json({ message: 'No token, authorization denied' });
